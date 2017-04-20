05d5cf3526c293f7377611a6e409c5775eccbba2
Maxime Franc <maxime.franc1@gmail.com>
2017-04-20 17:29:26 +0200

    WO exit process

--- srcs/ft_exit.c
+++ srcs/ft_exit.c
@@ -14,26 +14,32 @@
 
 void			ft_delcontroller(t_ctl **a_ctl, t_ctl **b_ctl)
 {
+	t_node	*a;
+	t_node	*b;
+
 	if (a_ctl)
 	{
-		if ((*a_ctl)->first)
+		a = (*a_ctl)->first;
+		if (a)
 		{
-			while ((*a_ctl)->first)
+			while (a)
 			{
-				ft_memdel((void**)(&(*a_ctl)->first));
-				(*a_ctl)->first = (*a_ctl)->first->next;
+				
+				free(a);
+				a = a->next;
 			}
 		}
 		free(a_ctl);
 	}
 	if (b_ctl)
 	{
-		if ((*a_ctl)->first)
+		b = (*b_ctl)->first;
+		if (b)
 		{
-			while ((*a_ctl)->first)
+			while (b)
 			{
-				ft_memdel((void**)(&(*a_ctl)->first));
-				(*a_ctl)->first = (*a_ctl)->first->next;
+				free(b);
+				b = b->next;
 			}
 		}
 		free(b_ctl);
@@ -42,18 +48,18 @@ void			ft_delcontroller(t_ctl **a_ctl, t_ctl **b_ctl)
 
 int			ft_exit_begin(t_ctl **a_ctl, t_ctl **b_ctl)
 {
-//	ft_delcontroller(a_ctl, b_ctl);
-	(void)a_ctl;
-	(void)b_ctl;
+	ft_delcontroller(a_ctl, b_ctl);
+//	(void)a_ctl;
+//	(void)b_ctl;
 	ft_putstrcolor("Error\n", RED);
 	return (-1);
 }
 
 int			ft_exit_parsing(t_ctl **a_ctl, t_ctl **b_ctl, char **tab)
 {
-//	ft_delcontroller(a_ctl, b_ctl);
-	(void)a_ctl;
-	(void)b_ctl;
+	ft_delcontroller(a_ctl, b_ctl);
+//	(void)a_ctl;
+//	(void)b_ctl;
 	if (ft_tablen(tab) != 0)
 		ft_tabdel(tab);
 	ft_putstrcolor("Error\n", RED);
@@ -62,9 +68,9 @@ int			ft_exit_parsing(t_ctl **a_ctl, t_ctl **b_ctl, char **tab)
 
 int			ft_exit_checker(t_ctl **a_ctl, t_ctl **b_ctl, char *line)
 {
-//	ft_delcontroller(a_ctl, b_ctl);
-	(void)a_ctl;
-	(void)b_ctl;
+	ft_delcontroller(a_ctl, b_ctl);
+//	(void)a_ctl;
+//	(void)b_ctl;
 	if (line)
 		ft_strdel(&line);
 	ft_putstrcolor("Error\n", RED);
