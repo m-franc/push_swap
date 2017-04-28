4193ffc03485b9fbaf5abef1584fa860e7f5b0b5
Maxime FRANC <mfranc@e2r5p19.42.fr>
2017-04-28 20:15:23 +0200

    WIP2

---
+++ srcs/ft_putnode_single.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnode.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mfranc <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/04/10 19:32:37 by mfranc            #+#    #+#             */
+/*   Updated: 2017/04/28 20:04:58 by mfranc           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+void		ft_putnode_single(t_node *node)
+{
+	if (!node)
+		return ;
+	ft_printf("data : {grey}%d{eoc} - data->prev : {cyan}NULL{eoc}\n",
+			node->data);
+	node = node->next;
+	while (node)
+	{
+		ft_printf("data : {grey}%d{eoc} - data->prev : {cyan}%d{eoc}\n",
+				node->data, node->prev->data);
+		node = node->next;
+	}
+}
