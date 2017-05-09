6c412d8de2cb3b2567ceab38f3f74d06ee19670e
Maxime FRANC <mfranc@e2r5p20.42.fr>
2017-05-09 19:07:54 +0200

    WIP

--- srcs/srcs_ps/push_swap.c
+++ srcs/srcs_ps/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: mfranc <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/18 17:04:23 by mfranc            #+#    #+#             */
-/*   Updated: 2017/05/08 15:35:04 by mfranc           ###   ########.fr       */
+/*   Updated: 2017/05/09 18:20:42 by mfranc           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,24 +38,64 @@ void		ft_split_stack(t_ctl *a_ctl, t_ctl *b_ctl)
 	ft_rrb(&b_ctl, 1);
 }
 
+int			ft_get_best_hit(int *index_a, t_ctl *a_ctl, t_ctl *b_ctl)
+{
+	t_node	*a;
+	t_node	*b;
+	int		hit;
+	int		pad_a;
+	int		pad_b;
+
+	hit = INT_MAX;
+	a = FIRST(a_ctl);
+	while (a)
+	{
+		pad_a = DST_TOP(a) >= DST_BOTTOM(a) ? DST_BOTTOM(a) : DST_TOP(a);
+		b = FIRST(b_ctl);
+		while (b && NEXT(b) && INDEX(a) < INDEX(b))
+			b = NEXT(b);
+		pad_b = DST_TOP(b) >= DST_BOTTOM(b) ? DST_BOTTOM(b) : DST_TOP(b);
+		if ((pad_a + pad_b) < hit)
+			*index_a = pad_a;
+		a = NEXT(a);
+	}
+	return (pad_b);
+}
+
 int			ft_push_swap(t_ctl *a_ctl, t_ctl *b_ctl)
 {
-	t_node	*max;
+	size_t	hit;
+	int		index_a;
 
+	ft_pb(&a_ctl, &b_ctl, 1);
+	ft_pb(&a_ctl, &b_ctl, 1);
+	index_a = 0;
 	while (SIZE(a_ctl) != 0)
 	{
-		max = ft_get_min_value(a_ctl);
-		if (DST_TOP(max) <= DST_BOTTOM(max))
-		{	
-			while (INDEX(FIRST(a_ctl)) != INDEX(max))
-				ft_ra(&a_ctl, 1);	
+		hit = ft_get_best_hit(&index_a, a_ctl, b_ctl);
+		if (index_a > (int)(SIZE(a_ctl) / 2))
+		{
+			while (index_a-- > 0)
+				ft_rra(&a_ctl, 1);
 			ft_pb(&a_ctl, &b_ctl, 1);
+			if (hit > (SIZE(a_ctl) / 2))
+				while (hit-- > 0)
+					ft_rb(&b_ctl, 1);
+			else
+				while (hit-- > 0)
+					ft_rrb(&b_ctl, 1);	
 		}
 		else
 		{
-			while (INDEX(FIRST(a_ctl)) != INDEX(max))
-				ft_rra(&a_ctl, 1);		
+			while (index_a-- > 0)
+				ft_ra(&a_ctl, 1);
 			ft_pb(&a_ctl, &b_ctl, 1);
+			if (hit > (SIZE(a_ctl) / 2))
+				while (hit-- > 0)
+					ft_rb(&b_ctl, 1);
+			else
+				while (hit-- > 0)
+					ft_rrb(&b_ctl, 1);	
 		}
 	}
 	while (SIZE(b_ctl) != 0)
